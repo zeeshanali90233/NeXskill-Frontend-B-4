5ec2ba4d4493838b8cd1f4dbe350d79dc3b82407
Zeeshan Ali <98347114+zeeshanali90233@users.noreply.github.com>
2024-11-02 03:21:06 -0700

    Saturday lec 10

--- JS/geoapi_weather_dom/index.js
+++ JS/geoapi_weather_dom/index.js
@@ -1,9 +1,73 @@
-function getLocation() {
+// GEO LOCATION API
+
+function getUserGeoLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((pos) => console.log(pos));
+    navigator.geolocation.getCurrentPosition(
+      (location) => {
+        console.log(location.coords.latitude);
+        console.log(location.coords.longitude);
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
   } else {
-    alert("Location NOt Available");
+    alert("Browser location not supported.");
+  }
+}
+async function getUserMedia() {
+  if (navigator.getUserMedia) {
+    const videoRef = document.querySelector(".cam");
+    const res = await navigator.getUserMedia(
+      {
+        audio: true,
+        video: { width: 200, height: 200 },
+      },
+      (stream) => {
+        videoRef.srcObject = stream;
+        videoRef.onloadedmetadata = (e) => {
+          videoRef.play();
+        };
+      },
+      (err) => {
+        console.error(`The following error occurred: ${err.name}`);
+      }
+    );
+  } else {
+    alert("Browser location not supported.");
   }
 }
 
-getLocation();
+// getUserGeoLocation();
+// getUserMedia();
+
+async function getWeather() {
+  const inputRef = document.querySelector(".cityName");
+  const resultDiv = document.querySelector(".result");
+  try {
+    const res = await fetch(
+      `https://p2pclouds.up.railway.app/v1/learn/weather?city=${inputRef.value}`,
+      {
+        method: "GET",
+      }
+    );
+    const weather = await res.json();
+    resultDiv.innerHTML = `
+      <div>
+        <p>Celcius: ${weather.current.temp_c}</p>
+      </div>
+      <div>
+        <p>Fahrenhiet: ${weather.current.temp_f}</p>
+      </div>
+    `;
+  } catch (err) {
+    console.log(err);
+    alert("Failed to fetch");
+  }
+}
+
+const canvasRef = document.getElementById("myCanvas");
+var ctx = canvasRef.getContext("2d");
+ctx.beginPath();
+ctx.arc(50, 10, 10, 0, Math.PI);
+ctx.stroke();
